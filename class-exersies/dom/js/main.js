b7a89ea68f254639b6a2293ff452a74c9ab6758a
Misrak-Seneserat <misrak.seneserat-ayele@sjsu.edu>
2025-09-21 19:49:11 -0700

    Update main.js

--- class-exersies/dom/js/main.js
+++ class-exersies/dom/js/main.js
@@ -1,7 +1,18 @@
 console.log("Helloo");
 
 
+let pageTitle = document.querySelector("#page-title");
 ///JAVASCRIPT Timed color change
-setTimeout(function(params) { document.querySelector("#page-title").style.color = "pink";
-    console.log("timeout worked");
+setTimeout(function(params) { pageTitle.style.color = "pink";
+   //console.log("timeout worked");
 }, 3000)
+
+///Click event on header change background
+document.querySelector("header").onclick = function(){
+    //console.log("clicked header");
+    document.querySelector("body").style.backgroundColor = "yellow"
+}
+
+
+
+
