1777b825535c1e1ac1311b2015d817102699c972
Misrak-Seneserat <misrak.seneserat-ayele@sjsu.edu>
2025-09-21 19:32:30 -0700

    uedip

---
+++ class-exersies/dom/js/main.js
@@ -0,0 +1,7 @@
+console.log("Helloo");
+
+
+///JAVASCRIPT Timed color change
+setTimeout(function(params) { document.querySelector("#page-title").style.color = "pink";
+    console.log("timeout worked");
+}, 3000)
