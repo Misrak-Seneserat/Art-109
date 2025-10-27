d1e2de0e492adbb59a2cba136c14ecaa78e6e469
Misrak-Seneserat <misrak.seneserat-ayele@sjsu.edu>
2025-10-27 15:00:14 -0700

    upodate

--- personal-site copy/js/sketch.js
+++ personal-site copy/js/sketch.js
@@ -5,7 +5,7 @@ function setup(){
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style("z-index", -2)
-    //background(225);
+    background(225);
 }
 
 function windowResized(){
@@ -18,25 +18,25 @@ function mouseMoved(){
  drawThing(mouseX - 50, mouseY + 75);
 }
 
-function drawThing(_x, _y){
-    strokeWeight(0);
-    fill(random(200,255), random(200,255), random(200,255));
-    ellipse(mouseX, mouseY, 30, 30);
+// function drawThing(_x, _y){
+//     strokeWeight(0);
+//     fill(random(200,255), random(200,255), random(200,255));
+//     ellipse(mouseX, mouseY, 30, 30);
 
-}
-
-
-function setup() { 
-  createCanvas(400, 400);
-} 
+// }
 
 
-function draw() { 
-  background(220);
-  for (let i=0; i<6; i++){
-    strokeWeight(0);
-    fill(255*(i/5)); 
-    ellipse(width/2, height/2, 300-(i*50), 300-(i*50));
-  }
+// function setup() { 
+//   createCanvas(400, 400);
+// } 
 
-}
+
+// function draw() { 
+//   background(220);
+//   for (let i=0; i<6; i++){
+//     strokeWeight(0);
+//     fill(255*(i/5)); 
+//     ellipse(width/2, height/2, 300-(i*50), 300-(i*50));
+//   }
+
+// }
