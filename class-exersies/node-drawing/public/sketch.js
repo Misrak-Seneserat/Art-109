774312ff52e8157394a83c311edbb7d024d171c6
Misrak-Seneserat <misrak.seneserat-ayele@sjsu.edu>
2025-11-10 23:30:07 -0800

    update

---
+++ class-exersies/node-drawing/public/sketch.js
@@ -0,0 +1,14 @@
+var socket;
+
+function setup() {
+    createCanvas(600, 400);
+    background(51)
+    socket = socket.io.connect('http://localhost:3000');
+}
+
+function draw() {
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 36, 36);
+ 
+}
