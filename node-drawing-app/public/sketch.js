a24399905e3045ca412e60af4c1fb62b50f3748c
Misrak-Seneserat <misrak.seneserat-ayele@sjsu.edu>
2025-11-11 01:29:19 -0800

    update

--- node-drawing-app/public/sketch.js
+++ node-drawing-app/public/sketch.js
@@ -1,13 +1,31 @@
 var socket;
 
 function setup() {
-creatCanvas(200, 200);
+creatCanvas(600, 400);
 background(51);
 
 socket = io.connect('http://localhost:3000')
+socket.on('mosue', newDrw);
+}
+
+function newDrawing(data){
+    noStroke();
+    fill(255, 0, 100);
+     ellipse(data.X, data.mosueY, 36, 36);
+}
+
+function mouseDragged(){
+    console.log('sending: ' + mosueX + ',' + mosueY);
+    var data = {
+        x: mosueX,
+        y: mosueY
+    }
+    socket.emit('mosue', data);
+
+     background(255);
+   
 }
 
 function draw() {
- background(51);
- ellipse(mosueX, mosueY, 60, 60);
+
 }
