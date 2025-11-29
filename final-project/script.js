a2267fe1606d2146f27f376fbb64bd2335564ad2
Misrak-Seneserat <misrak.seneserat-ayele@sjsu.edu>
2025-11-29 15:22:21 -0800

    new new

--- final-project/script.js
+++ final-project/script.js
@@ -11,14 +11,18 @@ function shake() {
 }
 
 function getFortune() {
-    var fortunes = ['Your Mom', 'Duck!', 'How did you even get in this situation?', 'How about NO?',
-        'Go ask someone who cares', 'The glass is half empty', 'Don’t ask me!', 'Just don’t bother', '404 - Quote not found',
-        'Wouldn’t you like to know! ? ', 'Don’t ask questions you don’t want to know the answer to.', 'Your guess is as good as mine',
+    var fortunes = ['Your Mom', 
+        'Duck!',
+         'How did you even get in this situation?', 
+         'How about NO?',
+        'Go ask someone who cares.', 'The glass is half empty.',
+         'Don’t ask me!', 'Just don’t bother.', '404 - Quote not found',
+        'Wouldn’t you like to know!', 'Don’t ask questions you don’t want to know the answer to.', 'Your guess is as good as mine',
         'I’m not nearly omnipotent enough for this', 'Search your feelings.You know it to be true.', 'Pay $1.99 for 5 more answers.',
         'Answer vague keep rubbing me.', 'You ARE the father!', 'Absolutely, positively, unequivocally, definitely maybe.',
-        'Reply hazy.Look it up on Google', 'Go ask a real person.', 'Lawyer Up', 'Oh, it’s you again',
+        'Reply hazy.Look it up on Google', 'Go ask a real person.', 'Lawyer Up', 'Oh, it’s you again...',
         'No, but someone else is having it much better than you.', 'You’re joking, right ?',
-        'We have been trying to contact you about your car’s extended warranty'];
+        'We have been trying to contact you about your car’s extended warranty!'];
         var fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
 
    
