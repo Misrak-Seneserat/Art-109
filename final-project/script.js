ae12a098a5871583abf8fa4e802aabd5688945f9
Misrak-Seneserat <misrak.seneserat-ayele@sjsu.edu>
2025-11-30 16:21:34 -0800

    new

--- final-project/script.js
+++ final-project/script.js
@@ -3,29 +3,42 @@ function shake() {
     var ball = document.getElementById("ball");
     ball.classList.add("shake");
 
-       setTimeout(function () {
+    setTimeout(function () {
         ball.classList.remove("shake");
-        getFortune();                       
-        document.getElementById("question").value = ""; 
+        getFortune();
+        document.getElementById("question").value = "";
     }, 1500);
 }
 
 function getFortune() {
-    var fortunes = ['Your Mom', 
+    var fortunes = [
+        'Your Mom',
         'Duck!',
-         'How did you even get in this situation?', 
-         'How about NO?',
-        'Go ask someone who cares.', 'The glass is half empty.',
-         'Don’t ask me!', 'Just don’t bother.', '404 - Quote not found',
-        'Wouldn’t you like to know!', 'Don’t ask questions you don’t want to know the answer to.', 'Your guess is as good as mine',
-        'I’m not nearly omnipotent enough for this', 'Search your feelings.You know it to be true.', 'Pay $1.99 for 5 more answers.',
-        'Answer vague keep rubbing me.', 'You ARE the father!', 'Absolutely, positively, unequivocally, definitely maybe.',
-        'Reply hazy.Look it up on Google', 'Go ask a real person.', 'Lawyer Up', 'Oh, it’s you again...',
-        'No, but someone else is having it much better than you.', 'You’re joking, right ?',
+        'How did you even get in this situation?',
+        'How about NO?',
+        'Go ask someone who cares.',
+        'The glass is half empty.',
+        'Don’t ask me!',
+        'Just don’t bother.',
+        '404 - Quote not found',
+        'Wouldn’t you like to know!',
+        'Don’t ask questions you don’t want to know the answer to.',
+        'Your guess is as good as mine',
+        'I’m not nearly omnipotent enough for this.',
+        'Search your feelings. You know it to be true.',
+        'Pay $1.99 for 5 more answers.',
+        'Answer vague, keep rubbing me.',
+        'You ARE the father!',
+        'Absolutely, positively, unequivocally, definitely maybe.',
+        'Reply hazy. Try Google?',
+        'Go ask a real person.',
+        'Lawyer Up', 'Oh, it’s you again...',
+        'No, but someone else is having it much better than you.',
+        'You’re joking, right?',
         'We have been trying to contact you about your car’s extended warranty!'];
-        var fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    var fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
 
-   
+
     var whiteSurface = document.querySelector(".whitesurface");
     whiteSurface.innerHTML = `<p class="ball-text" style="font-size:20px; padding:10px;">${fortune}</p>`;
 
