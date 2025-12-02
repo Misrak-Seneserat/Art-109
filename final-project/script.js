08ead0101de8d5be8df219ce7f429d44c3e3948a
Misrak-Seneserat <misrak.seneserat-ayele@sjsu.edu>
2025-12-01 16:37:20 -0800

    Update script.js

--- final-project/script.js
+++ final-project/script.js
@@ -17,10 +17,17 @@ function getFortune() {
         'How did you even get in this situation?',
         'How about NO?',
         'Go ask someone who cares.',
-        'The glass is half empty.',
+        'Depends on your state of mind.',
         'Don’t ask me!',
         'Just don’t bother.',
-        '404 - Quote not found',
+        '404 - ERROR',
+        'Budget Denied',
+        'You Wish!',
+        'Out of office',
+        'Discuss over drinks?',
+        'Get a grip!',
+        'If thats what you want',
+        'More coffee needed',
         'Wouldn’t you like to know!',
         'Don’t ask questions you don’t want to know the answer to.',
         'Your guess is as good as mine',
@@ -35,7 +42,8 @@ function getFortune() {
         'Lawyer Up', 'Oh, it’s you again...',
         'No, but someone else is having it much better than you.',
         'You’re joking, right?',
-        'We have been trying to contact you about your car’s extended warranty!'];
+        'We have been trying to contact you about your car’s extended warranty!'
+];
     var fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
 
 
